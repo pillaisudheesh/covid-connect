8300d2f274cfb88b1fc7b0140090a323fa7b3c1d
Sudheesh <sudheesh_pillai@hotmail.com>
2020-04-03 17:32:33 +0530

    Adds last update

--- src/store/actions/dashboardAction.js
+++ src/store/actions/dashboardAction.js
@@ -3,31 +3,54 @@ import covidData from '../data/covidTimelineData';
 
 
 
+
+function getCovidData(country, date, since) {
+    debugger;
+    const data = covidData;
+    let dateSince = new Date(date);
+    let payload = null;
+    payload = data.countries[country].filter(data => {
+        return data.date === date;
+    });
+    dateSince.setDate(dateSince.getDate() - since);
+    const dd = String(dateSince.getDate()).padStart(2, '0');
+    const mm = String(dateSince.getMonth() + 1).padStart(2, '0');
+    const yyyy = dateSince.getFullYear();
+    const dateDaysAgo = mm + '/' + dd + '/' + yyyy;
+    let oldData = null;
+    if (payload && payload.length > 0) {
+        oldData = data.countries[country].filter(data => {
+            return data.date === dateDaysAgo;
+        });
+    }
+    else {
+        dateSince = new Date(date);
+        dateSince.setDate(dateSince.getDate() - 1);
+        const dd = String(dateSince.getDate()).padStart(2, '0');
+        const mm = String(dateSince.getMonth() + 1).padStart(2, '0');
+        const yyyy = dateSince.getFullYear();
+        const dateOneDayAgo = mm + '/' + dd + '/' + yyyy;
+        payload = data.countries[country].filter(data => {
+            return data.date === dateOneDayAgo;
+        });
+        oldData = data.countries[country].filter(data => {
+            return data.date === dateDaysAgo;
+        });
+    }
+    let covidInfo = payload[0];
+    covidInfo.oldConfirmed = oldData[0].confirmed;
+    covidInfo.oldDeceased = oldData[0].deceased;
+    covidInfo.oldRecovered = oldData[0].recovered;
+    return covidInfo;
+}
+
 export function countryWideCovidInfo(country,date, since){
     return async dispatch => {
         debugger;
-        const data = covidData;
-        const currentDateValue = new Date(date);
-        currentDateValue.setDate(currentDateValue.getDate() - since);
-        const dd = String(currentDateValue.getDate()).padStart(2, '0');
-        const mm = String(currentDateValue.getMonth() + 1).padStart(2, '0'); 
-        const yyyy = currentDateValue.getFullYear();
-
-        const dateTwoDaysAgo = mm + '/' + dd + '/' + yyyy;
-
-        const payload = data.countries[country].filter(data => {
-            return data.date === date;
-        });
-
-        const oldData = data.countries[country].filter(data => {
-            return data.date === dateTwoDaysAgo;
-        });
-        let covidInfo = payload[0];
-        covidInfo.oldConfirmed = oldData[0].confirmed;
-        covidInfo.oldDeceased = oldData[0].deceased;
-        covidInfo.oldRecovered = oldData[0].recovered;
+        let covidInfo = getCovidData(country,date, since);
         dispatch(countryWideDataSuccess(covidInfo));
     }
+    
 }
 
 export function countryWideDataSuccess(payload){
