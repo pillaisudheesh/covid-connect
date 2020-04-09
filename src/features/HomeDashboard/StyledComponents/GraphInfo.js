60a221fd2ff9e7e59a58fdd9ffd35aced8e45131
Sudheesh Pillai <sudheesh_pillai@hotmail.com>
2020-04-09 18:13:38 +0530

    F/graph info (#18)
    * Removes unwanted import
    
    * Adds line graph

---
+++ src/features/HomeDashboard/StyledComponents/GraphInfo.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import GraphInfo from '../Components/GraphInfo';
+import theme from '../../../theme';
+
+export const GraphInfoContainer = styled(GraphInfo)`
+padding:32px;
+
+`;
--- src/store/reducers/dashboardReducer.js
+++ src/store/reducers/dashboardReducer.js
@@ -2,12 +2,15 @@ import * as types from '../actions/actionTypes';
 
 const initState = {
     convidInfo: null,
+    covidDataList: [],
   };
 
 export default function(state = initState, action) {
     switch (action.type) {
       case types.COUNTRY_WIDE_DATA:
         return { ...state, convidInfo: action.payload };
+      case types.COUNTRY_WIDE_DATA_LIST:
+        return { ...state, covidDataList: action.payload };
       default:
         return state;
     }
