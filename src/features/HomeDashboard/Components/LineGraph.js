025d941d76ce63204a7c9f41b24c213396b7ee7b
Sudheesh <sudheesh_pillai@hotmail.com>
2020-06-14 13:49:20 +0530

    Data update

--- src/features/HomeDashboard/Components/LineGraph.js
+++ src/features/HomeDashboard/Components/LineGraph.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import { Card, CardContent } from '@material-ui/core';
 
 export default function LineGraph (props) {
       return (
